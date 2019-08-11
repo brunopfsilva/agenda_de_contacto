a68c22225ec1c74e52cd42f0c7faaa1fa81749f0
bruno pf silva <bruno.pfsilva@hotmail.com>
2019-08-11 16:35:11 +0100

    conclusao da classe Contact com db

--- lib/helpers/contacto_helpers.dart
+++ lib/helpers/contacto_helpers.dart
@@ -1,6 +1,8 @@
-import 'package:sqflite/sql.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
@@ -8,6 +10,37 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 class ContactHelper {
+
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+
+  Future<Database>get db async {
+    if(db != null){
+      return _db;
+    }else {
+      _db = await initDb();
+    }
+    return _db;
+  }
+
+  Future<Database>initDb () async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath,"contacts.db");
+
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
+              "$phoneColumn TEXT, $imgColumn)"
+      );
+    });
+
+  }
 
 }
 
